161b0f18cb3ead4d89a9fa59a8cadc48fe94f464
0monish0 <monish.jod@gmail.com>
2026-04-10 14:18:44 +0530

    UC1

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * HelloApp - A simple Java application to demonstrate 
+ * console output and the standard main method signature.
+ */
+public class HelloApp {
+    
+    public static void main(String[] args) {
+        // Standard output stream to print message to console
+        System.out.println("Hello World");
+    }
+}
