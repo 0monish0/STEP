ec9f127e7eecfb7f8d49d06910c0481eaab74fff
0monish0 <monish.jod@gmail.com>
2026-04-13 14:38:53 +0530

    uc3

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Check if any arguments were provided
         if (args.length > 0) {
-            // Access the first element of the array
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+            System.out.println("Hello, " + args[0] + "!");
         } else {
-            // Fallback for UC1 behavior (Default Value)
-            System.out.println("Hello, World!");
+            System.out.println("Hello World");
         }
     }
 }
