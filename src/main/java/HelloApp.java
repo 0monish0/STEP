c68535a02e9a9e4924badd1374fe8e466afa7f90
0monish0 <monish.jod@gmail.com>
2026-04-13 14:44:15 +0530

    UC5

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,19 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            // Join all provided names with a space
-            String allNames = String.join(" ", args);
-            System.out.println("Hello, " + allNames + "!");
-        } else {
+        if (args.length == 0) {
             // Default message if no names are provided
             System.out.println("Hello, World!");
+        } else {
+            // Print the start of the greeting
+            System.out.print("Hello,");
+            
+            // The Enhanced For Loop
+            for (String arg : args) {
+                System.out.print(" " + arg);
+            }
+            
+            // Print the exclamation mark and move to a new line
+            System.out.println("!");
         }
     }
 }
